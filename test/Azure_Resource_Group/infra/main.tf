07e9b10b84796da273fd398c5d882e5f0f34af37
bhoopendra <devopsbs90@gmail.com>
2025-11-16 02:36:01 +0530

    new code

---
+++ test/Azure_Resource_Group/infra/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.49.0"
+    }
+  }
+
+}
+
+
+
+provider "azurerm" {
+  features {}
+  subscription_id = "f430c233-b9bc-4827-87f4-5d251ff61f32"
+}
+
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "bhuprg"
+  location = "west US"
+
+}
